a9b2f10856a303bf943307486d80acffff1b31bf
wickerman2019 <sverige@i.ua>
2023-01-22 10:07:53 +0200

    Simple if task

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        if(!false) {
+            System.out.println("true");
+        }
+
     }
 }
